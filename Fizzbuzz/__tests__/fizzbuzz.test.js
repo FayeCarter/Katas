b3ef5d26457d6afa09c675a336e50e3907ca0d48
FayeCarter <fayecarter333@gmail.com>
2020-06-22 09:31:03 +0100

    return Fizz when passed 3

--- Fizzbuzz/__tests__/fizzbuzz.test.js
+++ Fizzbuzz/__tests__/fizzbuzz.test.js
@@ -2,9 +2,13 @@ const { fizzbuzz } = require("../src/fizzbuzz");
 
 describe("Fizzbuzz", () => {
   
-  test("when passed a number not divisable by 3 or 5, returns number", () => {
+  test("when passed a number not divisible by 3 or 5, returns number", () => {
     expect(fizzbuzz(1)).toBe(1);
     expect(fizzbuzz(2)).toBe(2);
+  });
+
+  test("when passed a number divisible by 3, returns 'Fizz''", () => {
+    expect(fizzbuzz(3)).toBe("Fizz");
   });
 
 });
--- Fizzbuzz/src/fizzbuzz.js
+++ Fizzbuzz/src/fizzbuzz.js
@@ -1,4 +1,7 @@
 const fizzbuzz = (number) => {
+  if ( number % 3 === 0 ) {
+    return "Fizz"
+  }
   return number
 }
 
