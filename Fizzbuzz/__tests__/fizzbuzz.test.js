6cb723b40e0b59663d44aee5646824529e3c3e9d
FayeCarter <fayecarter333@gmail.com>
2020-06-22 09:32:19 +0100

    return Fizz when passed multiple of 3

--- Fizzbuzz/__tests__/fizzbuzz.test.js
+++ Fizzbuzz/__tests__/fizzbuzz.test.js
@@ -7,8 +7,9 @@ describe("Fizzbuzz", () => {
     expect(fizzbuzz(2)).toBe(2);
   });
 
-  test("when passed a number divisible by 3, returns 'Fizz''", () => {
+  test("when passed a number divisible by 3, returns 'Fizz'", () => {
     expect(fizzbuzz(3)).toBe("Fizz");
+    expect(fizzbuzz(6)).toBe("Fizz");
   });
 
 });
