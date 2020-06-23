e554d04d0c133c62aa165238518ad000ee00ffe2
FayeCarter <fayecarter333@gmail.com>
2020-06-23 09:21:10 +0100

    refactor character tests

--- RPG/__tests__/character.test.js
+++ RPG/__tests__/character.test.js
@@ -2,13 +2,17 @@ import Character from "../src/Character";
 
 describe("Character", () => {
   
+  let character;
+
+  beforeEach(() => {
+    character = new Character();
+  })
+
   test("Characters start with Health at 1000", () => {
-    const character = new Character()
     expect(character.health).toBe(1000)
   });
 
   test("Characters start at level 1", () => {
-    const character = new Character()
     expect(character.level).toBe(1)
   });
 
