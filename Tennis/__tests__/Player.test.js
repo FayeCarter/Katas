0997eb68cdea6cef272b4885e18cbba836e88033
FayeCarter <fayecarter333@gmail.com>
2020-06-26 09:09:44 +0100

    Player first score is 15

--- Tennis/__tests__/Player.test.js
+++ Tennis/__tests__/Player.test.js
@@ -5,4 +5,10 @@ describe("Player", () => {
     let player = new Player();
     expect(player.score).toBe(0);
   });
+
+  test("first score increases to 15", () => {
+    let player = new Player();
+    player.addScore();
+    expect(player.score).toBe(15);
+  });
 });
--- Tennis/src/Player.js
+++ Tennis/src/Player.js
@@ -2,4 +2,8 @@ export default class Game {
   constructor () {
     this.score = 0;
   };
+
+  addScore() {
+    this.score = 15;
+  }
 };
