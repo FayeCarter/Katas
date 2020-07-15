2ff32face1a70bd639b2b5b715ec4a666ab2fe58
FayeCarter <fayecarter333@gmail.com>
2020-07-15 09:14:33 +0100

    Bug Price check -toPound function

--- Supermarket/__tests__/PriceCheck.spec.js
+++ Supermarket/__tests__/PriceCheck.spec.js
@@ -1,8 +1,13 @@
 import PriceCheck from "../src/PriceCheck";
 
 describe("PriceCheck", () => {
-  it("A new PriceCheck calculates the price of 2 beans and returns a string", () => {
+  it("A new PriceCheck calculates the price of 2 beans and returns an array", () => {
     let priceCheck = new PriceCheck({ "beans": 2});
-    expect(priceCheck.calculate()).toEqual("beans 2 £0.40");
+    expect(priceCheck.calculate()).toEqual(["beans 2 £0.40"]);
+  });
+
+  it("A new PriceCheck calculates the price of 2 beans and 1 beer returns an array or items", () => {
+    let priceCheck = new PriceCheck({ "beans": 2, "beer": 1});
+    expect(priceCheck.calculate()).toEqual(["beans 2 £0.40, beer 1 £1.50"]);
   });
 });
--- Supermarket/src/PriceCheck.js
+++ Supermarket/src/PriceCheck.js
@@ -1,12 +1,23 @@
+import priceList from "./priceList"
+
 export default class PriceCheck {
   constructor(
     order
   ) {
     this.order = order;
+    this.priceList = [];
   };
-
+  
+  _toPounds (price) {
+    let conversion = price.toFixed(2)
+    return `£${conversion}`
+  };
   calculate() {
-    return "beans 2 £0.40"
+    for ( let item in this.order ) {
+      let price = priceList[item] * this.order[item]
+      price = _toPounds(price)
+      this.priceList.push(`${item} ${this.order[item]} ${price}`)
+    }
+    return this.priceList
   };
-    
 };
