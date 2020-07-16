c086387d113983b9d3440e461deccbffb137ec4b
FayeCarter <fayecarter333@gmail.com>
2020-07-16 08:50:36 +0100

    fizzbuzz returns Buzz

--- Python/fizzbuzz/fizzbuzz.py
+++ Python/fizzbuzz/fizzbuzz.py
@@ -1,4 +1,6 @@
 def fizzbuzz(number: int):
   if number % 3 == 0:
     return "Fizz"
+  elif number % 5 == 0:
+    return "Buzz"
   return number
--- Python/fizzbuzz/test_fizzbuzz.py
+++ Python/fizzbuzz/test_fizzbuzz.py
@@ -12,6 +12,14 @@ def test_should_return_Fizz():
   """
   For numbers not divisible by 3, fizzbuzz should return "Fizz"
   """
-  assert fizzbuzz(1) == 1
-  assert fizzbuzz(2) == 2
-  assert fizzbuzz(4) == 4
+  assert fizzbuzz(3) == "Fizz"
+  assert fizzbuzz(6) == "Fizz"
+  assert fizzbuzz(9) == "Fizz"
+
+def test_should_return_Buzz():
+  """
+  For numbers not divisible by 5, fizzbuzz should return "Buzz"
+  """
+  assert fizzbuzz(5) == "Buzz"
+  assert fizzbuzz(10) == "Buzz"
+  assert fizzbuzz(20) == "Buzz"
