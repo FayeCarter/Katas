528632be0eab1150b904be5d86e01ef7ad16fffd
FayeCarter <fayecarter333@gmail.com>
2020-07-16 09:13:06 +0100

    Player can't enter more than 5 dice

--- Python/Greed/greed.py
+++ Python/Greed/greed.py
@@ -1,3 +1,5 @@
 def greed(numbers):
   if len(numbers) < 5:
     return "You didn't roll renough dice"
+  elif len(numbers) > 5:
+    return "You rolled too many dice"
--- Python/Greed/test_greed.py
+++ Python/Greed/test_greed.py
@@ -5,3 +5,4 @@ def test_roll_5():
   Player must roll 5 dice
   """
   assert greed([1,2,3]) == "You didn't roll renough dice"
+  assert greed([1,2,3,4,5,6]) == "You rolled too many dice"
