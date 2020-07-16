0249df6bc3b4365399d2fe074016b8437e79314a
FayeCarter <fayecarter333@gmail.com>
2020-07-16 09:35:01 +0100

    4 1s returns 1100

--- Python/Greed/greed.py
+++ Python/Greed/greed.py
@@ -1,6 +1,8 @@
 def greed(numbers):
   if 1 in numbers:
-    if numbers.count(1) == 3:
+    if numbers.count(1) == 4:
+      return 1100
+    elif numbers.count(1) == 3:
       return 1000
     else:
       return 100
--- Python/Greed/test_greed.py
+++ Python/Greed/test_greed.py
@@ -7,10 +7,17 @@ def test_include_1():
   """
   assert greed([1,2,3,4,3]) == 100
 
-def test_include_tree_1s():
+def test_include_three_1s():
   """
   When players roll contains three 1s
   Return score of 1000
   """
   assert greed([1,2,1,1,3]) == 1000
+
+def test_include_four_1s():
+  """
+  When players roll contains four 1s
+  Return score of 1100
+  """
+  assert greed([1,1,1,1,3]) == 1100
 
